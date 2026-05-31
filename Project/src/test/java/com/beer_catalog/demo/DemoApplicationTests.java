51a060950adcb943a84c60aff85357e8b99a59b6
Arnav-003 <arnavbhakat4@gmail.com>
2026-05-31 20:46:11 +0530

    Adding the basic project structuer

---
+++ Project/src/test/java/com/beer_catalog/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer_catalog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
